2be8ed91e99ab32a5e6cbb7d6934ec5ac1c4d140
ega-septian <septian.ega@thelionparcel.com>
2026-04-20 01:44:14 +0700

    chore: enhance Playwright configuration with new project settings for Saucedemo tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,15 +25,25 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
-
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
 
   /* Configure projects for major browsers */
   projects: [
-    { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
+    {
+      name: 'chromium',
+      testIgnore: ['**/saucedemo/**', '**/TC-LP-*.spec.ts'],
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'saucedemo-chromium',
+      testDir: './tests',
+      testMatch: ['**/saucedemo/**/*.spec.ts', '**/TC-LP-*.spec.ts'],
+      use: {
+        ...devices['Desktop Chrome'],
+        baseURL: 'https://www.saucedemo.com',
+      },
+    },
   ],
 });
